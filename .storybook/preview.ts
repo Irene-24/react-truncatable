d5de665e1fa80f90f10d1a9bb96b7bf47b2435cb
Irene-24 <okaforirenen@gmail.com>
2023-09-09 19:04:27 +0100

    removing some extra stuff

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,8 +1,5 @@
 import type { Preview } from "@storybook/react";
 
-import { withThemeByClassName } from "@storybook/addon-styling";
-
-/* TODO: update import to your tailwind styles file. If you're using Angular, inject this through your angular.json config instead */
 import "../src/index.css";
 
 const preview: Preview = {
@@ -15,18 +12,6 @@ const preview: Preview = {
       },
     },
   },
-
-  decorators: [
-    // Adds theme switching support.
-    // NOTE: requires setting "darkMode" to "class" in your tailwind config
-    // withThemeByClassName({
-    //   themes: {
-    //     light: 'light',
-    //     dark: 'dark',
-    //   },
-    //   defaultTheme: 'light',
-    // }),
-  ],
 };
 
 export default preview;
